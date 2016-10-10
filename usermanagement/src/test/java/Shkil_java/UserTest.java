2216401d02c6d4b81230fdae30b72c5a7b587f18
Xedart <Xedpasmail@gmail.com>
2016-10-11 01:52:32 +0300

    init

---
+++ usermanagement/src/test/java/Shkil_java/UserTest.java
@@ -0,0 +1,40 @@
+package kn_14_5_Shkil;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase{
+	
+	private User user;
+	private Date dateOfBirthd;
+	
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		user = new User();
+		Calendar calendar= Calendar.getInstance();
+		calendar.set(1996, Calendar.NOVEMBER, 30);
+		dateOfBirthd = calendar.getTime();
+	}
+	
+	@Test
+	public void testGetFullName() {
+		user.setFirstName("Arthur");
+		user.setLastName("Shkil");
+		assertEquals("Shkil, Arthur", user.fullName());
+	}
+
+	public void testGetAge() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+		int currentYear = calendar.get(Calendar.YEAR);
+		user.setDateOfBirthd(dateOfBirthd);
+		assertEquals(currentYear-1996, user.getAge());
+	}
+}
